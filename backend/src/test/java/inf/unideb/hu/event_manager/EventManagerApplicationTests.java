2f64bffacecc0c3675d1f34f64a86e4342a59b10
BAndris4 <andrisbalogh444@gmail.com>
2025-11-17 19:24:33 +0100

    chore: initialize Spring Boot backend project

---
+++ backend/src/test/java/inf/unideb/hu/event_manager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package inf.unideb.hu.event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
